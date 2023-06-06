a54c9fd375f400ee0219319c5f01a55547090cab
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-06-07 03:19:31 +0900

    ✨ feat: 라우팅용 임시 페이지 추가

---
+++ pages/user/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header/header";
+import "@/app/globals.css";
+import Layout from "@/components/home/layout";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>임시 페이지</h2>
+      <Header />
+      <Layout />
+    </div>
+  );
+}
